a71c4772ebd706d9ce5ffd185b89462446fc85b1
李翔 <lx70770@gmail.com>
2019-07-24 11:09:57 +0800

    precommit test

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    node: true,
+    commonjs: true,
+    es6: true
+  },
+  globals: {
+    __DEBUG__: 'readonly',
+    __VERSION__: 'readonly'
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:jest/recommended',
+    'plugin:prettier/recommended'
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+      legacyDecorators: true
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: ['react', 'jest'],
+  rules: {
+    'no-control-regex': 0,
+    indent: ['error', 2, { SwitchCase: 1 }],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    'no-console': 'warn',
+    'react/jsx-uses-vars': 'error',
+    'react/prop-types': 0,
+    'react/display-name': 0
+  },
+  settings: {
+    react: {
+      version: '16.4.2'
+    }
+  }
+}
